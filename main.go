dd46d3ea7638a161b3dde56cbc63b784c45db8b0
sangshaofeng <shaofeng1022>
2019-01-28 15:18:13 +0800

    重新搭建目录结构

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	
+	_ "go-notebook/routers"
+	"github.com/astaxie/beego"
 )
 
 func main() {
-
+	beego.Run()
 }
+
--- routers/router.go
+++ routers/router.go
@@ -1,2 +1,10 @@
 package routers
 
+import (
+	"go-notebook/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
