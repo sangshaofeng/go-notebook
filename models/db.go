02242aff57611fcc6c7719f0e1d2439c14ac544c
shaofeng <3241113254@qq.com>
2019-02-12 18:45:55 +0800

    删除多余文件

--- models/db.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "github.com/astaxie/beego/orm"
-
-func init() {
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:shaofeng1022@tcp(127.0.0.1:3306)/go_notebook?charset=utf8", 30)
-}
