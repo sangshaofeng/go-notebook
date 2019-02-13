825d330d2f9968cba6ce3c7ef1fbd7035a6c6575
shaofeng <3241113254@qq.com>
2019-02-13 15:56:26 +0800

    utils和user model

--- models/user.go
+++ models/user.go
@@ -1,10 +1,11 @@
 package models
 
 import (
+	"go-notebook/utils"
 	"github.com/astaxie/beego/orm"
 )
  
-type Users struct {
+type User struct {
 	Id int
 	Username string
 	Password string
@@ -12,9 +13,25 @@ type Users struct {
 }
 
 func init() {
-	
+	orm.RegisterModel(new(User))
 }
 
+// 添加用户
 func AddUser(username string, password string) (int64, error) {
 	o := orm.NewOrm()
+	o.Using("default")
+	user := new(User)
+	user.Username = username
+	user.Salt = utils.RandString(10)
+	user.Password = utils.Md5(passord + user.Salt)
+	return o.Insert(user)
+}
+
+// 用户名查找用户
+func FindUser(username strng) (Users, error) {
+	o := orm.NewOrm()
+	o.Using("default")
+	user := Users{Username: username}
+	err := o.Read(&user, "username")
+	return user, err
 }
--- utils/functions.go
+++ utils/functions.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
-
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"io"
+	"fmt"
 )
 
 // 检查用户名是否合法，只接受数字和字母，且第一位非数字
@@ -18,4 +24,43 @@ func CheckUsernme(username string) bool {
 		}
 	}
 	return true
+}
+
+// 随机string
+func RandString(length int) string {
+	// 生成GUID(全局唯一标识符)
+	buf := make([]byte, 32)
+	var guid string
+	if _, err := io.ReadFull(rand.Reader, buf); err != nil{
+		guid = ""
+	} else {
+		guid = base64.URLEncoding.EncodeToString(buf)
+	}
+
+	// 截取指定的length长度的子串
+	rs := []rune(guid) // 强制转成int64类型
+	rsLen := len(rs)
+	begin := 0
+	if begin >= rsLen {
+		begin = rsLen
+	}
+	end := begin + length
+	if end > rsLen {
+		end = rsLen
+	}
+	return string(rs[begin:end])
+}
+
+// Md5加密，返回加密后的字符串
+func Md5(value string) string {
+	h := md5.New()
+	h.Write([]byte(value))
+	return fmt.Sprintf("%s", hex.EncodeToString(h.Sum(nil)))
+}
+
+// 对字符串进行sha1计算
+func Sha1(value string) string {
+	s := sha1.New()
+	io.WriteString(s, value)
+	return fmt.Sprintf("%x", s.Sum(nil))
 }
