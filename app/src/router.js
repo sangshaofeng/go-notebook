541d601f7e41baf9eb93a685d509f14596730165
shaofeng <3241113254@qq.com>
2019-02-20 19:14:27 +0800

    前端路由守卫

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import {checkoutLogin} from './utils/utils'
 
 Vue.use(Router)
 
@@ -9,7 +10,13 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {title: "首页"},
+      beforeEnter: (to, from, next) => {
+        if (!checkoutLogin()) {
+          next('/login')
+        } else next()
+      }
     },
     {
       path: '/login',
