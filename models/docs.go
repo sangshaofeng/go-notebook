be87d2322bfd096eb0af915d8d28076791e78e4d
shaofeng <3241113254@qq.com>
2019-02-25 19:05:43 +0800

    解决分页Bug

--- models/docs.go
+++ models/docs.go
@@ -4,6 +4,7 @@ package models
 import (
 	// "go-notebook/utils"
 	"github.com/astaxie/beego/orm"
+	"strconv"
 )
 
 // 文档
@@ -28,18 +29,21 @@ func init() {
 }
 
 // 获取全部文档目录
-func GetAllDocContent(page int) ([]orm.Params, int64, int64, error) {
+func GetAllDocContent(page int) ([]orm.Params, int64, int, error) {
 	o := orm.NewOrm()
 	sql := "select * from doc_contents order by created_at desc limit ?,10"
-	countSql := "select count(*) from doc_contents"
+	countSql := "select count(*) as number from doc_contents"
 	var maps, maps2 []orm.Params
 	num, err := o.Raw(sql, (page - 1) * 10).Values(&maps)
-	count, err := o.Raw(countSql).Values(&maps2)
-	var totalPages int64
-	if count % 10 == 0 {
-		totalPages = count % 10
+	// 注意下面这一行语句要使用 = 而不是使用 := ,因为 := 需要左侧有一个新的变量名字，
+	// 而匿名符号 _ 和 err 都不是新的变量名，所以会报错！
+	_, err = o.Raw(countSql).Values(&maps2)
+	totalCount, err := strconv.Atoi(maps2[0]["number"].(string))
+	var totalPages int
+	if (totalCount % 10) == 0 {
+		totalPages = totalCount / 10
 	} else {
-		totalPages = count % 10 + 1
+		totalPages = totalCount / 10 + 1
 	}
 	return maps, num, totalPages, err
 }
